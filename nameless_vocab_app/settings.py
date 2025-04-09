5570029a11e283b424c5f62eab17033607ea3650
Vyacheslav <parkthekingshady@gmail.com>
2025-04-09 22:34:25 +0900

    design

--- nameless_vocab_app/settings.py
+++ nameless_vocab_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ov($f=2*2&gmsuuuo$sfulb205o)*g_w604=)dx0(jvc3l0*oq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vocabapp-l7tj.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
@@ -113,14 +113,12 @@ USE_I18N = True
 USE_TZ = True
 
 LOGIN_REDIRECT_URL = '/'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# URL prefix for static files
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "/static/"
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
